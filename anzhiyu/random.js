2212d15757d2d69eae8343fb4a966b95e6cbc089
CN1008610010 <CN1001010086@outlook.com>
2025-01-10 22:07:42 +0800

    Site updated: 2025-01-10 22:07:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/04/这是另一篇文章/","2025/01/04/这是一篇测试文章/"];function toRandomPost(){
+var posts=[];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
