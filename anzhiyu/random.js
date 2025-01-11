603e3c7a6517b299155b510c6e6b6e4aa45b705c
CN1008610010 <CN1001010086@outlook.com>
2025-01-11 19:44:58 +0800

    Site updated: 2025-01-11 19:44:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/11/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2025/01/11/今天是周六/","2025/01/11/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
