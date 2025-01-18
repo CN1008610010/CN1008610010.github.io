08615ce6e0aacced04a81c0c9fa1a771d77c2815
CN1008610010 <CN1001010086@outlook.com>
2025-01-18 21:55:10 +0800

    Site updated: 2025-01-18 21:55:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/11/今天是周六/"];function toRandomPost(){
+var posts=["2025/01/18/假期第一篇/","2025/01/11/今天是周六/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
