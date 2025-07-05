f7f94e9ade683d0053cc173b57c776eec9a9e361
CN1008610010 <CN1001010086@outlook.com>
2025-07-05 15:51:27 +0800

    Site updated: 2025-07-05 15:51:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/18/假期第一篇/","2025/01/11/今天是周六/"];function toRandomPost(){
+var posts=["2025/07/05/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
